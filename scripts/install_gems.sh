bfb5590cbf687129adac20ba12f56d10d5132f43
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 01:37:43 +0900

    Update

--- scripts/install_gems.sh
+++ scripts/install_gems.sh
@@ -4,6 +4,4 @@ gem install bundler
 gem install sinatra
 gem install rack
 
-cd /var/www/sinatra
-echo `pwd`
-bundle install --path vendor/
+echo `whoami`
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo `whoami`
+
 cd /var/www/sinatra
 bundle install --path vendor
 bundle exec rackup -p 8080 -o 0.0.0.0 -P rack.pid
