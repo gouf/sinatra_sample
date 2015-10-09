0b40cff1e2ddc62230815b4891f9e69e495d9895
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 03:24:20 +0900

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo `whoami`
+echo `pwd`
 
 export PATH="/usr/local/bin:$PATH"
 cd /var/www/sinatra
+echo `pwd`
+gem install io-console
 gem install bundler
 echo `which bundle`
-bundle install --path vendor
-bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
+~/bin/bundle install --path vendor
+~/bin/bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
