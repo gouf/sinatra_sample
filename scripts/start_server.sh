14e77eef1bf23282c45a3355a12a11b196950ed7
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 02:38:59 +0900

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 
 echo `whoami`
 
+export PATH="/usr/local/bin:$PATH"
 cd /var/www/sinatra
 source /usr/local/rvm/scripts/rvm
-rackup -p 8080 -o 0.0.0.0 -P rack.pid
+bundle exec rackup -p 8080 -o 0.0.0.0 -P rack.pid -D
