1ea7333f1a6dca1b7f784c93bb343b3772ac14a1
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 03:29:27 +0900

    Update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,7 @@ yum install -y ruby
 yum install -y gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
 yum install -y ruby-rdoc ruby-devel
 yum install -y rubygems
+yum install -y git
 
 httpd=`pgrep httpd`
 
