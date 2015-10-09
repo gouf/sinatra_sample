721bce1cccd6e584ca1b8d80cb3228a19945f0b5
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 02:51:44 +0900

    Fix binding port

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,4 +6,4 @@ export PATH="/usr/local/bin:$PATH"
 cd /var/www/sinatra
 source /usr/local/rvm/scripts/rvm
 bundle install --path vendor
-bundle exec rackup -p 8080 -o 0.0.0.0 -P rack.pid -D
+bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
