5b01b89f0a9408719f1e4ed71a3820104ab72e43
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 02:23:32 +0900

    Install io-console

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,6 +5,7 @@ echo `whoami`
 cd /var/www/sinatra
 source /usr/local/rvm/scripts/rvm
 rvm --version
+gem install io-console
 gem install bundler
 bundle install --path vendor
 bundle exec rackup -p 8080 -o 0.0.0.0 -P rack.pid
