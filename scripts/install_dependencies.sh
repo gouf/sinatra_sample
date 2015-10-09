e242d336b7f270ec2205aee34c94c11cb5b97a36
Gou Furuya <innocent.zero@gmail.com>
2015-10-09 22:03:20 +0900

    Remove bundler install lines

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,3 @@ yum install -y ruby
 yum install -y gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
 yum install -y ruby-rdoc ruby-devel
 yum install -y rubygems
-
-gem install bundler
-bundle
