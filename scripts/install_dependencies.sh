144bb236d8738f8dc864b7322939751cd7e9d8b5
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 01:59:16 +0900

    Update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,3 +4,6 @@ yum install -y ruby
 yum install -y gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
 yum install -y ruby-rdoc ruby-devel
 yum install -y rubygems
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,8 @@
 echo `whoami`
 
 cd /var/www/sinatra
+source /usr/local/rvm/scripts/rvm
+rvm --version
 gem install bundler
 bundle install --path vendor
 bundle exec rackup -p 8080 -o 0.0.0.0 -P rack.pid
