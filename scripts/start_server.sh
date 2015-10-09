719d8b9235b86a521f3b320390b4a6cd1f4ea37e
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 03:14:31 +0900

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,7 @@ echo `whoami`
 
 export PATH="/usr/local/bin:$PATH"
 cd /var/www/sinatra
+gem install bundler
+echo `which bundle`
 bundle install --path vendor
 bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
