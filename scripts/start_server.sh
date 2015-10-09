bd18f2bd47c98ef42b92ce4c464a8e5a772c7992
Gou Furuya <innocent.zero@gmail.com>
2015-10-10 03:50:59 +0900

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,7 +7,12 @@ export PATH="/usr/local/bin:$PATH"
 cd /var/www/sinatra
 echo `pwd`
 gem install io-console
+gem install rack
 gem install bundler
 echo `which bundle`
-~/bin/bundle install --path ~/vendor
-~/bin/bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
+# ~/bin/bundle install --path ~/vendor
+# ~/bin/bundle exec rackup -p 80 -o 0.0.0.0 -P rack.pid -D
+if [[ -f ~/bin/bundle ]]; then
+  bundle install
+fi
+rackup -p 80 -o 0.0.0.0 -P rack.pid -D
